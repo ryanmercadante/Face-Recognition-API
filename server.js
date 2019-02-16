f822099724fb889a4df0a21dfb2ef8edcd114843
Ryan Mercadante <ryan.a.mercadante@gmail.com>
2019-02-15 22:50:23 -0500

    create temporary signin and register endpoints, but without a db

--- server.js
+++ server.js
@@ -1,9 +1,56 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
+app.use(bodyParser.json());
+const db = {
+  users: [
+    {
+      id: "123",
+      name: "John",
+      email: "john@gmail.com",
+      password: "cookies",
+      entries: 0,
+      joined: new Date()
+    },
+    {
+      id: "124",
+      name: "Sally",
+      email: "sally@gmail.com",
+      password: "bananas",
+      entries: 0,
+      joined: new Date()
+    }
+  ]
+};
+
 app.get("/", (req, res) => {
-  res.send("this is working");
+  res.send(db.users);
+});
+
+app.post("/signin", (req, res) => {
+  if (
+    req.body.email === db.users[0].email &&
+    req.body.password === db.users[0].password
+  ) {
+    res.json("success");
+  } else {
+    res.status(400).json("error logging in");
+  }
+});
+
+app.post("/register", (req, res) => {
+  const { email, name, password } = req.body;
+  db.users.push({
+    id: "125",
+    name: name,
+    email: email,
+    password: password,
+    entries: 0,
+    joined: new Date()
+  });
+  res.json(db.users[db.users.length - 1]);
 });
 
 app.listen(3000, () => {
